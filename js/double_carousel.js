fe69294aa9bd06aef8b175075bc906caf22fa221
rdunning75 <ryandunning57@gmail.com>
2019-01-18 00:37:42 -0800

    added individual pictures for leaders

--- js/double_carousel.js
+++ js/double_carousel.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
 
-    var slider = $('.owl-carousel');
-    slider.owlCarousel({
+    var slider1 = $('.double_carousel1');
+    slider1.owlCarousel({
+        center: true,
+        items: 1,
+        nav: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause:true,
+        smartSpeed: 1500,
+        margin: 0,
+        navText: ['<i class="fas fa-chevron-left trans_300"></i>','<i class="fas fa-chevron-right trans_300"></i>']
+    });
+
+    var slider2 = $('.double_carousel2');
+    slider2.owlCarousel({
         center: true,
         items: 1,
         nav: true,
         loop: true,
         margin: 0
-    })
+    });
+
 
 });
